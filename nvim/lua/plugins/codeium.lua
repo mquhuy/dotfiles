ea43d56a8848dea42645429d0c93e3ccc3bcbb3e
Huy Mai <huy.mai@est.tech>
2023-02-01 21:52:14 +0200

    Install nvim-codeium

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -1,3 +1,12 @@
 return {
-  'Exafunction/codeium.vim',
+    "jcdickinson/codeium.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        require("codeium").setup({
+        })
+    end
 }
