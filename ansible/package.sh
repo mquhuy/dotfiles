31cd863d03cfe054a6c37134a2efa8b996fe0eb5
Huy Mai <huy.mai@est.tech>
2023-02-01 21:49:03 +0200

    Update

---
+++ ansible/package.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k.git
+ln -s $PWD/../zsh/.p10k.sh ~
