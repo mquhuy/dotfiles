88c6cfd1a7a9872b2dd7b98e8a2d8c4f527277f1
Huy Mai <huy.mai@est.tech>
2023-02-04 20:14:33 +0200

    Update tmux conf

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -30,6 +30,6 @@ if [ ! -f $HOME/.tmux.conf ]; then
 	ln -s $__dir__/tmux/tmux.conf $HOME/.tmux.conf
 	rm -rf $HOME/.tmux
 	mkdir -p $HOME/.tmux
-	ln -s $__dir__/tmux/themes $HOME/.tmux/themes
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
