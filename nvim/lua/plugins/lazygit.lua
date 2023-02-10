c1bc880721fc2e51e1d212a20c30e5b19d25252a
Huy Mai <huy.mai@est.tech>
2023-02-10 08:48:35 +0200

    Update

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -3,13 +3,12 @@ return {
   keys = {
     {"<leader>gg", "<cmd>LazyGit<cr>", desc="lazygit"},
   },
-  config = {
-    silent = true,
-    lazygit_floating_window_winblend = 0, -- transparency of floating window
-    lazygit_floating_window_scaling_factor = 0.9, -- scaling factor for floating window
-    lazygit_floating_window_use_plenary = 0, -- use plenary.nvim to manage floating window if available
-    lazygit_use_neovim_remote = 1, -- fallback to 0 if neovim-remote is not installed
-    lazygit_use_custom_config_file_path = 0, -- config file path is evaluated if this value is 1
-    lazygit_config_file_path = '', -- custom config file path
-  },
+  config = function ()
+    lazygit_floating_window_winblend = 0 -- transparency of floating window
+    lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floating window
+    lazygit_floating_window_use_plenary = 0 -- use plenary.nvim to manage floating window if available
+    lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote is not installed
+    lazygit_use_custom_config_file_path = 0 -- config file path is evaluated if this value is 1
+    lazygit_config_file_path = '' -- custom config file path
+  end
 }
