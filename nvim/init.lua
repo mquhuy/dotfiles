4934471d674c39663cc6bfed0c2ffaa8e7ea151e
Huy Mai <huy.mai@est.tech>
2023-02-11 22:20:29 +0200

    Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -344,7 +344,7 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
-    { name = "codeium" },
+    -- { name = "codeium" },
   },
 }
 
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -25,9 +25,6 @@ return {
   --Magit
   -- 'jreybert/vimagit',
 
-  -- Fuzzy Finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
-
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = vim.fn.executable 'make' == 1 },
 
