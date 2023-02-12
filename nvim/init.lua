e4dc531e4cbab5314b9ae225170544042600c127
Huy Mai <huy.mai@est.tech>
2023-02-12 10:20:59 +0200

    Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -117,22 +117,6 @@ require('gitsigns').setup {
   },
 }
 
--- [[ Configure Telescope ]]
--- See `:help telescope` and `:help telescope.setup()`
-require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-      n = {
-        ['d'] = 'delete_buffer',
-      },
-    },
-  },
-}
-
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,26 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.1',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function ()
+    -- [[ Configure Telescope ]]
+    -- See `:help telescope` and `:help telescope.setup()`
+    require('telescope').setup {
+      defaults = {
+        mappings = {
+          i = {
+            ['<C-u>'] = false,
+            ['<C-d>'] = false,
+          },
+          n = {
+            ['d'] = 'delete_buffer',
+          },
+        },
+      },
+    }
+  end
+}
