fbb8599d65fc3d49e582d19cd6b04eda808e9b97
Huy Mai <huy.mai@est.tech>
2023-02-12 09:25:30 +0200

    Revert "Improve lua syntax"
    This reverts commit 8b03ed1e091eb9a5cb272b8e556fa3f747ad9ba9.

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,4 +1,19 @@
 return {
+  { -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+      'j-hui/fidget.nvim',
+
+      -- Additional lua configuration, makes nvim stuff amazing
+      'folke/neodev.nvim',
+    },
+  },
+
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
@@ -30,6 +45,4 @@ return {
 
   --tmux
   'aserowy/tmux.nvim',
-
-  'mquhuy/dendron.nvim',
 }
