de6aaf925ae2da0602bbcd4d96b89506cbef93f7
Huy Mai <huy.mai@est.tech>
2023-03-23 09:21:34 +0200

    Improve clone script

--- bin_lib/fzf_lib.sh
+++ bin_lib/fzf_lib.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 LIST_FILES_DIR="$DOTFILES_DIR/TxtLists"
+choose() {
+    LIST_FILE=$1
+    LIST_FILE="${LIST_FILES_DIR}/${LIST_FILE}"
+    ITEM=$2
+    exists=$(awk -F';' '{print $1}' $LIST_FILE | grep $ITEM)
+    if [[ $exists == "" ]]; then
+        exit 0
+    fi
+    link=$(grep $ITEM $LIST_FILE | awk -F';' '{print $2}')
+    echo $link
+}
+
 fzf_choose() {
     LIST_FILE=$1
     LIST_FILE="${LIST_FILES_DIR}/${LIST_FILE}"
