b6274409dfc9e7444a23a6f585d3a7ef8c698e3b
Huy Mai <huy.mai@est.tech>
2023-04-12 09:30:01 +0300

    Add nvim installer

---
+++ installers/install_nvim.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+mkdir -p $HOME/Programs
+
+rm -rf $HOME/.local/bin/nvim $HOME/Programs/nvim
+
+version="stable"
+
+filename="nvim-linux64"
+
+wget -O $HOME/Programs/$filename.tar.gz "https://github.com/neovim/neovim/releases/download/${version}/${filename}.tar.gz"
+
+tar -xf $HOME/Programs/$filename.tar.gz -C $HOME/Programs
+
+mv $HOME/Programs/$filename $HOME/Programs/nvim
+
+rm -f $HOME/Programs/$filename.tar.gz
+ln -s $HOME/Programs/nvim/bin/nvim $HOME/.local/bin/nvim
