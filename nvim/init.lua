5562de23ce839abf58e2bb01e868d98cf6fae48e
Huy Mai <huy.mai@est.tech>
2023-04-13 09:09:25 +0300

    Update telescope_diff keybinds

--- nvim/init.lua
+++ nvim/init.lua
@@ -145,6 +145,12 @@ vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { des
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', "<leader>sC", function()
+  require("telescope").extensions.diff.diff_files({ hidden = true })
+end, { desc = "Compare 2 files" })
+vim.keymap.set('n', "<leader>sc", function()
+  require("telescope").extensions.diff.diff_current({ hidden = true })
+end, { desc = "Compare file with current" })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
