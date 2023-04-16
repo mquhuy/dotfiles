5a1bb626d8d9d8fb6065cdd7b79974467b99a017
Huy Mai <huy.mai@est.tech>
2023-04-16 11:05:35 +0300

    Update i3_installer

--- installers/install_i3.sh
+++ installers/install_i3.sh
@@ -7,7 +7,7 @@ libxcb-util0-dev libxcb-icccm4-dev libyajl-dev \
 libstartup-notification0-dev libxcb-randr0-dev \
 libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
 libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev \
-autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
+autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev --y
 
 DIR=$HOME/Programs/i3
 git clone https://github.com/i3/i3 $DIR
@@ -18,3 +18,5 @@ ninja
 
 sudo rm -r /usr/bin/i3*
 sudo cp i3* /usr/bin
+
+sudo apt install --reinstall i3blocks
