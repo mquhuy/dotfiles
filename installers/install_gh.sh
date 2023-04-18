1d5bb0c455195a1b9b60b578c70d304e7c143442
Huy Mai <huy.mai@est.tech>
2023-04-18 12:33:50 +0300

    Fix install_gh

--- installers/install_gh.sh
+++ installers/install_gh.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-source common_lib.sh
+__dir__=$(realpath $(dirname $0))
+source $__dir__/common_lib.sh
 mkdir -p $HOME/Programs
 
 rm -rf $HOME/.local/bin/gh $HOME/Programs/gh
@@ -8,7 +9,6 @@ rm -rf $HOME/.local/bin/gh $HOME/Programs/gh
 version=$(get_latest_release "cli" "cli")
 
 filename="gh_$(echo $version | sed 's/v//g')_linux_amd64"
-
 wget -O $HOME/Programs/$filename.tar.gz "https://github.com/cli/cli/releases/download/${version}/${filename}.tar.gz"
 
 tar -xf $HOME/Programs/$filename.tar.gz -C $HOME/Programs
