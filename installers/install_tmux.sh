c803095b1ea65af6d40fa01416981a8aee12c64c
Cloud User <cloud-user@huy-new-big.openstacklocal>
2023-04-27 03:36:31 -0400

    Update install_tmux

--- installers/install_tmux.sh
+++ installers/install_tmux.sh
@@ -1,6 +1,6 @@
-rm -f $HOME/.tmux.conf
-ln -s $__dir__/tmux/tmux.conf $HOME/.tmux.conf
 rm -rf $HOME/.tmux
 mkdir -p $HOME/.tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-$HOME/.tmux/plugins/tpm/bin/install_plugins.sh
+rm -f $HOME/.tmux.conf
+ln -s $__dir__/tmux/tmux.conf $HOME/.tmux.conf
+$HOME/.tmux/plugins/tpm/bin/install_plugins
