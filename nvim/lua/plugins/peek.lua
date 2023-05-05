ec6d2ab60dcf57e72c07dd645bdfde0fa89729ff
Huy Mai <huy.mai@est.tech>
2023-05-05 14:52:52 +0300

    Update peek

--- nvim/lua/plugins/peek.lua
+++ nvim/lua/plugins/peek.lua
@@ -1,10 +1,23 @@
 return {
   'toppair/peek.nvim',
   lazy = true,
-  init = function ()
-    os.execute('deno task --quiet build:fast')
+  build = function()
+     os.execute("deno task build:fast")
   end,
   opts = {
     filetype = { 'markdown', 'telekasten' },
   },
+  keys = {
+    {"<leader>p", function()
+     local peek = require('peek')
+     if not peek.is_open() then
+       vim.fn.system('i3-msg split horizontal')
+       peek.open()
+     else
+      peek.close()
+      vim.fn.system('i3-msg move left')
+     end
+    end
+    },
+  }
 }
