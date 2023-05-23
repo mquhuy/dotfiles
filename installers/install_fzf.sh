6bb74df6be982a8b974f19d789d3385e9c3a8670
Huy Mai <huy.mai@est.tech>
2023-05-23 02:10:18 -0400

    Add install fzf

---
+++ installers/install_fzf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+__dir__=$(realpath $(dirname $0))
+source $__dir__/common_lib.sh
+mkdir -p $HOME/Programs
+
+rm -rf $HOME/.local/bin/gh $HOME/Programs/gh
+user="junegunn"
+repo="fzf"
+
+version=$(get_latest_release ${user} ${repo})
+
+filename="fzf-${version}-linux_amd64"
+wget -O $HOME/Programs/$filename.tar.gz "https://github.com/${user}/${repo}/releases/download/${version}/${filename}.tar.gz"
+
+tar -xf $HOME/Programs/$filename.tar.gz -C $HOME/Programs
+
+rm -f $HOME/Programs/$filename.tar.gz
+ln -s $HOME/Programs/fzf $HOME/.local/bin/fzf
