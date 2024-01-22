89339a17ab3691ff53808dd833f86c1634d9739d
Huy Mai <huy.mai@est.tech>
2024-01-22 14:08:14 +0000

    Update

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -4,9 +4,6 @@ return {
     "ray-x/guihua.lua",
     "neovim/nvim-lspconfig",
     "nvim-treesitter/nvim-treesitter",
-    "mfussenegger/nvim-dap",
-    "mfussenegger/nvim-dap-ui",
-    "theHamsta/nvim-dap-virtual-text",
   },
   config = function()
     require("go").setup()
