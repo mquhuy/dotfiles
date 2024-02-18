12b7b280e705cd80be7ef07f9fbffa3d52169dc4
Huy Mai <huy.mai@est.tech>
2024-02-18 20:02:39 +0200

    Minor change in wiki nvim
    Signed-off-by: Huy Mai <huy.mai@est.tech>

--- nvim-writing/lua/plugins/headlines.lua
+++ nvim-writing/lua/plugins/headlines.lua
@@ -1,5 +1,24 @@
 return {
     "lukas-reineke/headlines.nvim",
     dependencies = "nvim-treesitter/nvim-treesitter",
-    config = true, -- or `opts = {}`
+    -- config = true, -- or `opts = {}`
+    opts = {
+        markdown = {
+            headline_highlights = {
+                "Headline1",
+                "Headline2"
+                },
+            fat_headlines = false,
+            fat_headline_upper_string = "▃",
+            --
+            codeblock_highlight = "CodeBlock",
+            --
+            quote_highlight = "Quote",
+            quote_string = "┃",
+            -- quote_string = ">> ",
+            --
+            dash_highlight = "Dash",
+            dash_string = "_",
+        },
+    }
 }
