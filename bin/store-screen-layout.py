d1c9ecb04435272ad8dde31567aca561166c2ef6
Huy Mai <huy.mai@est.tech>
2024-02-25 16:52:50 +0200

    Improve screen store
    Signed-off-by: Huy Mai <huy.mai@est.tech>

--- bin/store-screen-layout.py
+++ bin/store-screen-layout.py
@@ -7,6 +7,7 @@ import os
 import sys
 
 MONITOR_FILE = os.path.join(os.environ["HOME"], ".screenlayout", "monitors.json")
+BASE_COMMAND = ["i3-msg", "-t"]
 
 def load_layout():
     if os.path.exists(MONITOR_FILE):
@@ -19,36 +20,41 @@ def save_layout(layout):
     with open(MONITOR_FILE, "w") as f:
         json.dump(layout, f, indent=4, sort_keys=True)
 
-class Monitor:
-    def __init__(self, connect_line, mode_line):
-        connect_content = connect_line.split()
-        self.name = connect_content[0]
-        self.mode = connect_content[2:]
-        self.identity = mode_line.strip().split()[0]
+def get_workspaces():
+    workspaces = json.loads(subprocess.check_output(BASE_COMMAND + ["get_workspaces"]).decode("utf-8"))
+    return workspaces
+
+def get_workspaces_by_outputs():
+    workspaces = get_workspaces()
+    data = {}
+    for workspace in workspaces:
+        data[workspace["output"]] = data.get(workspace["output"], []) + [workspace["num"]]
+    return data
 
 def get_active_monitors():
-    active_monitor_output=subprocess.check_output(["xrandr"]).decode("utf-8").split("\n")
-    active_monitors=[]
-    for idx, line in enumerate(active_monitor_output):
-        if re.match("^\S+ connected", line):
-            active_monitors.append(Monitor(line, active_monitor_output[idx+1]))
-    return active_monitors
+    active_monitors=json.loads(subprocess.check_output(BASE_COMMAND + ["get_outputs"]).decode("utf-8"))
+    active_monitors = [monitor for monitor in active_monitors if monitor["name"] != "xroot-0"]
+    workspaces_by_monitors = get_workspaces_by_outputs()
+    data = {}
+    for monitor in active_monitors:
+        name = monitor["name"]
+        monitor.update(workspaces=workspaces_by_monitors.get(name, []))
+        data[name] = monitor
+    return data
 
 def get_setup_name():
     active_monitors=get_active_monitors()
-    name = "-".join([monitor.identity for monitor in active_monitors])
+    name = "-".join([monitor["name"] for monitor in active_monitors.values()])
     return name
 
 def set_display_off_with_filter(filter=[]):
     # Only keep the screens that are in the filter
     store_layout()
-    # command = ["xrandr", "--output", "eDP-1", "--mode", "1920x1200", "--pos", "0x0", "--primary", "--rotate", "normal"]
-    # subprocess.run(command)
     active_monitors = get_active_monitors()
-    for monitor in active_monitors:
-        if monitor.name in filter:
+    for name in active_monitors:
+        if name in filter:
             continue
-        command = ["xrandr", "--output", monitor.name, "--off"]
+        command = ["xrandr", "--output", name, "--off"]
         subprocess.run(command)
 
 def set_laptop_mode():
@@ -61,15 +67,9 @@ def store_layout():
     monitors = load_layout()
 
     active_monitors = get_active_monitors()
-
-    setup_info = {}
-
-    for monitor in active_monitors:
-        setup_info[monitor.identity] = monitor.mode
-
     setup_name = get_setup_name()
 
-    monitors[setup_name] = setup_info
+    monitors[setup_name] = active_monitors
     save_layout(monitors)
 
 def restore_layout():
@@ -83,24 +83,33 @@ def restore_layout():
 
     setup_info = monitors[setup_name]
     active_monitors = get_active_monitors()
+    workspaces = get_workspaces()
+    workspace_nums = [ws["num"] for ws in workspaces]
+    print(workspace_nums)
 
-    for monitor in active_monitors:
-        if monitor.identity not in setup_info:
-            print("Monitor {} not found in stored layout".format(monitor.name))
+    for name in active_monitors:
+        if name not in setup_info:
+            print("Monitor {} not found in stored layout".format(name))
             set_laptop_mode()
             return
+        monitor = setup_info[name]
         command = ["xrandr"]
-        monitor_mode = setup_info[monitor.identity]
-        command += ["--output", monitor.name]
-        idx = 0
-        if monitor_mode[idx] == "primary":
+        monitor_info = setup_info[name]
+        command += ["--output", name]
+        if monitor_info["primary"]:
             command += ["--primary"]
-            idx += 1
-        mode_info = monitor_mode[idx].split("+")
-        command += ["--mode", mode_info[0], "--pos", mode_info[1] + "x" + mode_info[2]]
+        monitor_mode = monitor_info["rect"]
+        command += ["--mode", f"{monitor_mode['width']}x{monitor_mode['height']}", "--pos", f"{monitor_mode['x']}x{monitor_mode['y']}"]
         if len(command) > 1:
             print(command)
             subprocess.run(command)
+        if monitor == "eDP-1":
+            continue
+        for workspace in monitor["workspaces"]:
+            if workspace in workspace_nums:
+                command = ["i3-msg", "move", "workspace", "number", str(workspace), "to", "output", monitor["name"]]
+                print(command)
+                subprocess.run(command)
 
 if __name__ == "__main__":
     if sys.argv[1] == "store":
