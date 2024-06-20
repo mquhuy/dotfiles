c797260d9773fb8e088037090d5057d7b5fca3e9
Huy Mai <huy.mai@est.tech>
2024-06-20 14:00:03 +0300

    Improve kmonad keyboard detection
    Signed-off-by: Huy Mai <huy.mai@est.tech>

--- bin/choose_kb.sh
+++ bin/choose_kb.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/env bash
+#
 KMONAD_CONFIG_DIR=${DOTFILES_DIR}/kmonad
 chosen_keyboard=$(ls ${KMONAD_CONFIG_DIR}/*.org | fzf)
 emacs --batch --eval "(require 'org)" --eval "(org-babel-tangle-file \"${chosen_keyboard}\")"
+device_name="$(grep -oP '(?<=input \(device-file \").*(?=\")' ~/.config/kmonad/config.kbd)"
+if [[ "$device_name" != "/dev/*" ]]; then
+    export device_name
+    python3 << 'END_PYTHON'
+import os
+import re
+
+device_name = os.environ['device_name']
+print(device_name)
+with open('/proc/bus/input/devices') as f:
+    devices = f.read().split('\n\n')
+    for device in devices:
+        if device_name in device:
+            device_file = re.search(r'event\d+', device).group()
+            if device_file:
+                with open(os.path.expanduser('~/.config/kmonad/config.kbd'), 'r') as f:
+                    config = f.read()
+                config = config.replace(device_name, f'/dev/input/{device_file}')
+                with open(os.path.expanduser('~/.config/kmonad/config.kbd'), 'w') as f:
+                    f.write(config)
+            break
+END_PYTHON
+fi
 systemctl --user restart kmonad
