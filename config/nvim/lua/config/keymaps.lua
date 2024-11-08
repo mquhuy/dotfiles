663a5d8b021e33ff9bdea6c52c84d905b194ce40
Huy Mai <huy.mai@est.tech>
2024-11-08 12:29:59 +0200

    Fix Oil keymap
    Signed-off-by: Huy Mai <huy.mai@est.tech>

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,4 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-map("n", "-", "<cmd>Oil<cr>", { desc = "Oil edit" })
+vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Oil edit" })
