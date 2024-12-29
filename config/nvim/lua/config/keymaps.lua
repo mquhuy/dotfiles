b9ac09d069991e32dac1c0abda6f69e2aea78f7e
Huy Mai <me@huymai.fi>
2024-12-29 14:37:35 +0200

    Update lazyvim keymaps
    Signed-off-by: Huy Mai <me@huymai.fi>

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Oil edit" })
+vim.keymap.set("n", "<leader>fw", require("telescope.builtin").grep_string, { desc = "[F]ind current [W]ord" })
+vim.keymap.del("n", "<leader>fg")
+vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, { desc = "[F]ind by [G]rep" })
