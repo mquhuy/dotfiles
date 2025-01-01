046e736ac64c4a5a2feb204dedec21e73872dc72
Huy Mai <huy.mai@est.tech>
2025-01-01 21:28:40 +0200

    Update
    Signed-off-by: Huy Mai <huy.mai@est.tech>

--- installers/install_nvm.sh
+++ installers/install_nvm.sh
@@ -6,6 +6,6 @@ source $__dir__/common_lib.sh
 version=$(get_latest_release "nvm-sh" "nvm")
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${version}/install.sh | bash
 
-source ~/.bashrc
+source ~/.zshrc
 nvm install --lts
 nvm use --lts
