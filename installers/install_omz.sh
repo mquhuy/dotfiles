a4453e64850ea97c50f98a55a0307c9c9f9a2615
Huy Mai <huy.mai@est.tech>
2025-01-20 15:48:20 +0200

    Update install_omz
    Signed-off-by: Huy Mai <huy.mai@est.tech>

--- installers/install_omz.sh
+++ installers/install_omz.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-		rm -f $HOME/.zshrc $HOME/.zshrc.pre-oh-my-zsh
-		ln -s $__dir__/zsh/zshrc $HOME/.zshrc
-		git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
-		git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/themes/powerlevel10k
-		rm -f $HOME/.p10k.zsh
-		ln -s $__dir__/zsh/p10k.zsh $HOME/.p10k.zsh
-
+__dir__=$(realpath $(dirname $0))
+if [[ ! -d $HOME/.oh-my-zsh ]]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+rm -f "${HOME}/.zshrc" "${HOME}/.zshrc.pre-oh-my-zsh"
+ln -s "${__dir__}/../zsh/zshrc" "${HOME}/.zshrc"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting "${HOME}/.oh-my-zsh/plugins/zsh-syntax-highlighting"
+git clone https://github.com/romkatv/powerlevel10k.git "${HOME}/.oh-my-zsh/themes/powerlevel10k"
+rm -f "${HOME}/.p10k.zsh"
+ln -s "${__dir__}/../zsh/p10k.zsh" "${HOME}/.p10k.zsh"
