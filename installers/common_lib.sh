6f5afa53cf24310cd94b565ae42f70cbc82d1791
Huy Mai <huy.mai@est.tech>
2025-03-10 10:10:32 +0200

    Improve get_latest_release to only take stable versions
    Signed-off-by: Huy Mai <huy.mai@est.tech>

--- installers/common_lib.sh
+++ installers/common_lib.sh
@@ -6,7 +6,7 @@ function get_latest_release() {
   api="https://api.github.com/repos/${org}/${repo}/releases"
   set +x
   curl -sL "${api}" > releases.txt
-  release_tag="$(cat releases.txt | jq -r ".[].tag_name" | head -n 1 )"
+  release_tag="$(cat releases.txt | jq -r '.[] | select(.prerelease == false) | .tag_name' | head -n 1 )"
 
   echo "$release_tag"
   rm -f releases.txt
