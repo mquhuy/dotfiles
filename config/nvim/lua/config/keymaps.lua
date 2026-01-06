aba13373ffd345d96cd32b2472d9cdd46a335d01
Huy Mai <me@huymai.fi>
2026-01-06 05:39:13 +0200

    Update configurations for i3, kitty, nvim, zsh and installers
    - i3: Change kill window binding to Mod1+q
    - kitty: Add extensive configuration for window management and tmux-like bindings
    - nvim: Update plugin locks, remove telescope keymaps
    - zsh: Add prompt, history, and completion settings
    - playbooks: Update kitty and nvim installers, fix nvim download URL

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,19 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Oil edit" })
-vim.keymap.set("n", "<leader>fw", require("telescope.builtin").grep_string, { desc = "[F]ind current [W]ord" })
-vim.keymap.del("n", "<leader>fg")
-vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, { desc = "[F]ind by [G]rep" })
-vim.keymap.del("n", "<leader><space>")
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.del("n", "<leader>/")
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer]' })
 vim.keymap.set({ 'n' }, '<Leader>te', function()
   vim.cmd('tabedit %')
 end, { silent = true, noremap = true, desc = '[T]ab [E]dit' })
