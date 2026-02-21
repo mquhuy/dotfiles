9f649a970c7116302650a2e9bc22398663c9c640
Huy Mai <me@huymai.fi>
2026-02-21 15:09:04 +0200

    Resolve merge conflicts

--- bin/default-browser.sh
+++ bin/default-browser.sh
@@ -8,9 +8,9 @@ PROFILE_DIR="${HOME_DIR}/.local/profiles/${PROFILE_NAME}"
 
 mkdir -p "${HOME_DIR}/.local/profiles"
 
-if [[ ! -d "${PROFILE_DIR}" ]]; then
-    cp -r ~/.config/BraveSoftware/Brave-Browser "${PROFILE_DIR}"
-fi
+# if [[ ! -d "${PROFILE_DIR}" ]]; then
+    # cp -r ~/.config/BraveSoftware/Brave-Browser "${PROFILE_DIR}"
+# fi
 mkdir -p "${PROFILE_DIR}"
 
 "${DEFAULT_BROWSER}" --user-data-dir="${PROFILE_DIR}" --window-name="${PROFILE_NAME}" "${SITE}" > /dev/null
